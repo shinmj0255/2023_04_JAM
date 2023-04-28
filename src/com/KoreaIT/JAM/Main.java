7b3223f46988454253a467decc6b0a0681f17cb3
shinmj0255 <shinmj0255@gmail.com>
2023-04-28 21:03:51 +0900

    게시물 리스팅 시 데이터를 DB로 부터 가져오기, 게시물 수정 기능 구현

--- src/com/KoreaIT/JAM/Main.java
+++ src/com/KoreaIT/JAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,8 +14,6 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		
 		Scanner sc = new Scanner(System.in);
-		
-		List<Article> articles = new ArrayList<>();
 		
 		while(true) {
 			System.out.printf("명령어) ");
@@ -32,9 +31,6 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				
-//				Article article = new Article(id, title, body);
-//				articles.add(article);
 				
 				Connection conn = null;
 				PreparedStatement pstmt = null;
@@ -81,6 +77,64 @@ public class Main {
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
 				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+
+				List<Article> articles = new ArrayList<>();
+				
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC";
+					
+					pstmt = conn.prepareStatement(sql);
+					rs = pstmt.executeQuery();
+					
+					while(rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+						
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+					
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
 				if (articles.size() == 0) {
 					System.out.println("존재하는 게시물이 없습니다");
 					continue;
@@ -91,6 +145,56 @@ public class Main {
 				for (Article article : articles) {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
+			} else if (cmd.startsWith("article modify ")) {
+				int id = Integer.parseInt(cmd.split(" ")[2]);
+				System.out.printf("== %d번 게시글 수정 ==\n", id);
+				
+				System.out.printf("수정할 제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("수정할 내용 : ");
+				String body = sc.nextLine();
+				
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/jdbc_article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					
+					String sql = "UPDATE article";
+					sql += " SET updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "'";
+					sql += " WHERE id = " + id;
+					
+					pstmt = conn.prepareStatement(sql);
+					pstmt.executeUpdate();
+					
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				System.out.printf("%d번 게시글이 수정되었습니다\n", id);
 			}
 		}
 		
